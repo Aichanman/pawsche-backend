658cd26c9efb7c0cf0c2f9bac89542b6c505607d
Aichanman <izg03420@kwansei.ac.jp>
2026-07-08 23:22:49 +0900

    Update friends.js

--- api/friends.js
+++ api/friends.js
@@ -1,9 +1,6 @@
 import { Redis } from '@upstash/redis';
 
-const redis = new Redis({
-  url: process.env.KV_URL,
-  token: process.env.KV_REST_API_TOKEN,
-});
+const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
   const origin = req.headers.origin || '';
